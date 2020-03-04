209bafdd2dfaadd125f01ad1e1029c332d1129be
junior-jacob <60950047+junior-jacob@users.noreply.github.com>
2020-03-03 22:15:06 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+#autore : 1dsi3 
+#@junior
+#feito em 03/03/2020 
+#ISNTITUIÇÃO ETEC
+
+x = 0 # decalraçao de comando 
+valor = 2 #declaraçao da variavel valor 
+while (x <= 21) :
+	    valor = valor * valor 
+	    x = x + 1 # laço de repetiçao 
+print (valor) # Exibir a variavael valor 	    
